a508a7ebe73b429f317c6a3ad2d980dead958d28
hahy23 <gkswntjr2329@gmail.com>
2023-12-08 18:14:55 +0900

    롬복 설치로 BookDTO 클래스 @DATA 사용

--- restfulAPI/src/main/java/com/application/restfulAPI/book/controller/BookController.java
+++ restfulAPI/src/main/java/com/application/restfulAPI/book/controller/BookController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ import com.application.restfulAPI.response.dto.StatusCode;
 
 
 @RestController
+@RequestMapping(produces = "application/json; charset=utf8")
 public class BookController {
 	
 	@Autowired
@@ -28,42 +30,43 @@ public class BookController {
 	
 	
 	@GetMapping("/books") //전체 조회 : 책 관련 전체 조회
-	public List<BookDTO> booksGet() throws Exception{
+	public ResponseEntity<List<BookDTO>> booksGet() throws Exception{
 		
-		return bookService.getBooksInfo();
+		
+		return new ResponseEntity<List<BookDTO>>(bookService.getBooksInfo(),HttpStatus.OK);
 		
 	}
 	
 	@GetMapping("/books/{bookId}") // 부분 조회 : 고유 ID를 통한 부분 조회 (PathVariable 사용)
-	public BookDTO booksGet(@PathVariable long bookId) throws Exception{
+	public ResponseEntity<BookDTO> booksGet(@PathVariable long bookId) throws Exception{
 		
 		BookDTO bookDTO = bookService.getBookIdInfo(bookId);
 		
-		return bookDTO;
+		return new ResponseEntity<BookDTO>(bookDTO,HttpStatus.OK);
 		
 	}
 	
 	@PostMapping("/books") //데이터 생성 : bookName, bookAuthor, bookPrice를 쿼리 스트링으로 입력 후 요청 시  bookDTO객체에 저장하여 DB저장 
-	public BookDTO booksPost(@ModelAttribute BookDTO bookDTO) throws Exception{
+	public ResponseEntity<BookDTO> booksPost(@ModelAttribute BookDTO bookDTO) throws Exception{
 		
 		bookService.insertBookInfo(bookDTO);
 		
-		return bookService.getBooksNameInfo(bookDTO); // DB에 저장이 되었는지 확인을 위해 사용자에게 해당 데이터를 전달해준다.
+		return new ResponseEntity<BookDTO>(bookService.getBooksNameInfo(bookDTO), HttpStatus.OK); // DB에 저장이 되었는지 확인을 위해 사용자에게 해당 데이터를 전달해준다.
 		
 	}
 	
 	@PutMapping("/books/{bookId}") //전체 수정 : 책의 고유 ID를 통한 해당 정보를 전체 수정한다.
-	public BookDTO booksPut(@PathVariable long bookId, @ModelAttribute BookDTO bookDTO) throws Exception{
+	public ResponseEntity<BookDTO> booksPut(@PathVariable long bookId, @ModelAttribute BookDTO bookDTO) throws Exception{
 		
 		bookDTO.setBookId(bookId);
 		
 		bookService.updateBookInfo(bookDTO);
 		
-		return bookService.getBookIdInfo(bookId); //DB에 수정이 제대로 이루어졌는지 확인을 위해 데이터를 전달해준다.
+		return new ResponseEntity<BookDTO>(bookService.getBookIdInfo(bookId), HttpStatus.OK); //DB에 수정이 제대로 이루어졌는지 확인을 위해 데이터를 전달해준다.
 	}
 	
 	@PatchMapping("/books/{bookId}") //부분 수정 : 책의 고유 ID를 통해 책 이름을 수정한다.
-	public BookDTO booksPatch(@PathVariable long bookId, @RequestParam("bookName") String bookName) throws Exception{
+	public ResponseEntity<BookDTO> booksPatch(@PathVariable long bookId, @RequestParam("bookName") String bookName) throws Exception{
 		
 		BookDTO bookDTO = new BookDTO();
 		bookDTO.setBookId(bookId);
@@ -71,7 +74,7 @@ public class BookController {
 		
 		bookService.updateBookName(bookDTO);
 		
-		return bookService.getBookIdInfo(bookId); //DB에 수정이 제대로 이루어졌는지 확인을 위해 데이터를 전달해준다.
+		return new ResponseEntity<BookDTO>(bookService.getBookIdInfo(bookId), HttpStatus.OK); //DB에 수정이 제대로 이루어졌는지 확인을 위해 데이터를 전달해준다.
 	}
 	
 	@DeleteMapping("/books/{bookId}")
--- restfulAPI/src/main/java/com/application/restfulAPI/book/dto/BookDTO.java
+++ restfulAPI/src/main/java/com/application/restfulAPI/book/dto/BookDTO.java
@@ -1,45 +1,16 @@
 package com.application.restfulAPI.book.dto;
 
-import org.springframework.stereotype.Component;
 
-@Component
+
+import lombok.Data;
+
+@Data
 public class BookDTO {
 	
 	private long bookId;
 	private String bookName;
 	private String bookAuthor;
 	private String bookPrice;
-	
-	public long getBookId() {
-		return bookId;
-	}
-	public void setBookId(long bookId) {
-		this.bookId = bookId;
-	}
-	public String getBookName() {
-		return bookName;
-	}
-	public void setBookName(String bookName) {
-		this.bookName = bookName;
-	}
-	public String getBookAuthor() {
-		return bookAuthor;
-	}
-	public void setBookAuthor(String bookAuthor) {
-		this.bookAuthor = bookAuthor;
-	}
-	public String getBookPrice() {
-		return bookPrice;
-	}
-	public void setBookPrice(String bookPrice) {
-		this.bookPrice = bookPrice;
-	}
-	@Override
-	public String toString() {
-		return "BookDTO [bookId=" + bookId + ", bookName=" + bookName + ", bookAuthor=" + bookAuthor + ", bookPrice="
-				+ bookPrice + ", toString()=" + super.toString() + "]";
-	}
-	
 	
 	
 	
